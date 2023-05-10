09dfdb38dee2a0e6d59928428e1851fdcd788bfa
Kapil Chaudhary <kc685838@gmail.com>
2023-05-10 12:45:49 +0530

    submitting code-Wed May 10 2023 12:45:47 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
-
 import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  function greeting(event) {
+    event.preventDefault();
+    let greet = event.target.value;
+    if(greet != "") {
+      document.getElementById('greeting').innerHTML = `Hello ${greet}!`;
+    } else {
+      document.getElementById('greeting').innerHTML = "";
+    }
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <form>
+          <label for="name">Enter your name:</label>
+          <br />
+          <input id="name" type="text" onInput={greeting} />
+        </form>
+        <div id="greeting"></div>
     </div>
   )
 }
